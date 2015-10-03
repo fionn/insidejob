97ecab48465f1fcc0cd0c1fe3dcc222d0bd292b3
Fionn Fitzmaurice <iamfionn@github.com>
2015-10-03 16:21:55 +0100

    blacklist, ignore tweets I've already retweeted

--- insidejob.py
+++ insidejob.py
@@ -4,33 +4,32 @@ import tweepy
 from creds import consumer_key, consumer_secret, access_token, access_token_secret
 from time import sleep
 from html import unescape
-from datetime import datetime
-import re
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
 def criteria(tweet):
-    if ("RT" in tweet.text) or ("t.co" in tweet.text) or (len(tweet.text) > 40):
+    if ("RT" in tweet.text) or ("t.co" in tweet.text) or (len(tweet.text) > 48):
         return False
-    if "happiness" in tweet.text.lower():
+
+    if api.get_status(tweet.id).retweeted:
         return False
-    for element in ["inside", "job"]:
-        if (element in tweet.user.screen_name.lower() or
-            re.search("@[a-zA-Z0-9_]*" + element + "[a-zA-Z0-9_]*", tweet.text.lower())):
-            return False
+
     return True
 
 
 def retweet():
-    search = "\"an inside job\""
+    blacklist = open("blacklist.txt", "r").read().splitlines()
+    search = "\"an inside job\" -" + " -".join(blacklist)
+
     tweets = api.search(q=search, lang='en', count=100)
     tweets[:] = [tweet for tweet in tweets if criteria(tweet)]
-    tweet = tweets.pop(0)
+
+    tweet = tweets.pop()
 
     print("\"" + unescape(tweet.text) + "\" from @" + tweet.user.screen_name, 
-            "at " + str(tweet.created_at) + " UTC")
+            "at", str(tweet.created_at), "UTC (" + str(len(tweets)), "left)")
 
     api.retweet(tweet.id)
 
@@ -39,7 +38,10 @@ while True:
         retweet()
         sleep(60 * 60)
     except tweepy.error.TweepError as e:
-        print(e)
+        if e.response.status_code == 403:
+            print("I've already retweeted that")
+        else:
+            print(e)
         sleep(15 * 60)
     except IndexError as e:      # in case the search results
         print("IndexError:", e)  # list is empty
